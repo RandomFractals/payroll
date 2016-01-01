7e25993c82cec6b4bc6611fd703db8e3972d6200
Taras Novak <taras.novak@randomfractals.com>
2016-01-01 15:44:40 -0600

    created db migrations

---
+++ src/payroll/Migrations/20160101211451_Initial.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+using Microsoft.Data.Entity.Metadata;
+
+namespace payroll.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    PersonID = table.Column<int>(nullable: false),
+                    Salary = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeID);
+                });
+            migrationBuilder.CreateTable(
+                name: "Dependent",
+                columns: table => new
+                {
+                    DependentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmployeeEmployeeID = table.Column<int>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    PersonID = table.Column<int>(nullable: false),
+                    Relationship = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dependent", x => x.DependentID);
+                    table.ForeignKey(
+                        name: "FK_Dependent_Employee_EmployeeEmployeeID",
+                        column: x => x.EmployeeEmployeeID,
+                        principalTable: "Employee",
+                        principalColumn: "EmployeeID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Dependent");
+            migrationBuilder.DropTable("Employee");
+        }
+    }
+}
---
+++ src/payroll/Migrations/EmployeeContextModelSnapshot.cs
@@ -0,0 +1,65 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using payroll.Models;
+
+namespace payroll.Migrations
+{
+    [DbContext(typeof(EmployeeContext))]
+    partial class EmployeeContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("payroll.Models.Dependent", b =>
+                {
+                    b.Property<int>("DependentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("EmployeeEmployeeID");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<string>("MiddleName");
+
+                    b.Property<int>("PersonID");
+
+                    b.Property<int>("Relationship");
+
+                    b.HasKey("DependentID");
+                });
+
+            modelBuilder.Entity("payroll.Models.Employee", b =>
+                {
+                    b.Property<int>("EmployeeID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<string>("MiddleName");
+
+                    b.Property<int>("PersonID");
+
+                    b.Property<decimal>("Salary");
+
+                    b.HasKey("EmployeeID");
+                });
+
+            modelBuilder.Entity("payroll.Models.Dependent", b =>
+                {
+                    b.HasOne("payroll.Models.Employee")
+                        .WithMany()
+                        .HasForeignKey("EmployeeEmployeeID");
+                });
+        }
+    }
+}
