afaabdf658ba9698cde61c442a0c7c7cd749655c
Taras Novak <taras.novak@randomfractals.com>
2016-01-01 13:21:01 -0600

    created employee, dependent, and relationship cs

---
+++ src/payroll/Models/Relationship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace payroll.Models
+{
+    public enum Relationship
+    {
+        [Display(Name = "daughter")]
+        Daughter = 0,
+
+        [Display(Name = "son")]
+        Son = 1,
+
+        [Display(Name = "spouse")]
+        Spouse = 2
+    }
+}
