b7a1038a2e09df395f104b8d066825ba5efcdb12
Taras Novak <taras.novak@randomfractals.com>
2016-01-01 16:52:23 -0600

    added employee controller and view setup

---
+++ src/payroll/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+using payroll.Models;
+
+namespace payroll.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [FromServices]
+        public EmployeeContext EmployeeContext { get; set; }        
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var employees = EmployeeContext.Employees;
+
+            return View(employees);
+        }
+    }
+}
