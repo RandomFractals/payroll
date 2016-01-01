a3a478c13ee58edeaf91274b692fd98d4b7905bf
Taras Novak <taras.novak@randomfractals.com>
2016-01-01 12:55:52 -0600

    initial asp.net 5 + EF7 app setup + Person model

---
+++ src/payroll/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace payroll.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set;  }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set;  }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set;  }
+
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+    }
+}
