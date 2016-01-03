82cd16e80d65259f7459be67fb10ee34ea01f713
Taras Novak <taras.novak@randomfractals.com>
2016-01-03 13:01:17 -0600

    added dependent deductions calc

--- src/payroll/Models/Employee.cs
+++ src/payroll/Models/Employee.cs
@@ -6,9 +6,9 @@ namespace payroll.Models
     public class Employee : Person
     {
         // TODO: move these business rules to payroll service facade later
-        public static decimal DefaultSalary = 2000 * 26; // $2000/check * 26 checks/year
-        public static decimal DeductionPerEmployee = 1000; // $1000/year
-        public static decimal DeductionPerDependent = 500; // $500/year
+        public static decimal DefaultSalary = 2000m * 26; // $2000/check * 26 checks/year
+        public static decimal DeductionPerEmployee = 1000m; // $1000/year
+        public static decimal DeductionPerDependent = 500m; // $500/year
 
         public int EmployeeID { get; set; }
 
@@ -30,19 +30,28 @@ namespace payroll.Models
             get
             {
                 // TODO: move this business logic to payroll service facade later
-                _deduction = DeductionPerEmployee;
+                _deductions = DeductionPerEmployee;
                 if ( FirstName.StartsWith("A") )
                 {
-                    _deduction *= .01m; // 10% discount
+                    _deductions *= .9m; // 10% discount
                 }
 
-                return _deduction;
+                foreach(Dependent dependent in Dependents)
+                {
+                    decimal dependentDeduction = DeductionPerDependent;
+                    if ( dependent.FirstName.StartsWith("A") )
+                    {
+                        dependentDeduction *= .9m; // 10% discount
+                    }
+                    _deductions += dependentDeduction;
+                }
+                return _deductions;
             }
             set {
-                _deduction = value;
+                _deductions = value;
             }
         }
-        private decimal _deduction;
+        private decimal _deductions;
 
 
         public Employee()
