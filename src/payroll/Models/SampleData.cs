eebc496aff9947555cd08ba0f2f5f80b580cb295
Taras Novak <taras.novak@randomfractals.com>
2016-01-03 00:44:17 -0600

    fixed sample data

--- src/payroll/Models/SampleData.cs
+++ src/payroll/Models/SampleData.cs
@@ -12,30 +12,34 @@ namespace payroll.Models
 		{
 
             var context = serviceProvider.GetService<EmployeeDataContext>();
+
 			if ( serviceProvider.GetService<IRelationalDatabaseCreator>().Exists() )
 			{
-				var janeDoe = context.Employees.Add(
-					new Employee { FirstName = "Jane", LastName = "Doe", Salary = 52000}).Entity;
 
-                context.Dependents.AddRange(
-                    new Dependent()
-                    {
-                        FirstName = "Jon",
-                        LastName = "Don",
-                        Employee = janeDoe,
-                        Relationship = Relationship.Spouse
-                    },
-                    new Dependent()
-                    {
-                        FirstName = "Sally",
-                        MiddleName = "Madison",
-                        LastName = "Don-Doe",
-                        Relationship = Relationship.Daughter
-                    }
-                );
+                if (!context.Employees.Any())
+                {
+                    var janeDoe = context.Employees.Add(
+                    new Employee { FirstName = "Jane", LastName = "Doe", Salary = 52000 }).Entity;
 
-                context.SaveChanges();
-					
+                    context.Dependents.AddRange(
+                        new Dependent()
+                        {
+                            FirstName = "Jon",
+                            LastName = "Don",
+                            Employee = janeDoe,
+                            Relationship = Relationship.Spouse
+                        },
+                        new Dependent()
+                        {
+                            FirstName = "Sally",
+                            MiddleName = "Madison",
+                            LastName = "Don-Doe",
+                            Relationship = Relationship.Daughter
+                        }
+                    );
+
+                    context.SaveChanges();
+                }					
 			}
 		}
     }
