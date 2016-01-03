af62ec47e1b3bb5867c1670d1ce8d491b8082b82
Taras Novak <taras.novak@randomfractals.com>
2016-01-02 21:01:05 -0600

    dependents list routing changes +  add dependent updates

---
+++ src/payroll/Controllers/DependentController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Logging;
+
+using payroll.Models;
+
+namespace payroll.Controllers
+{
+    public class DependentController : Controller
+    {
+
+        [FromServices]
+        public EmployeeDataContext EmployeeDataContext { get; set; }
+
+
+        public async Task<ActionResult> EmployeeInfo(int id)
+        {
+            Employee employee = await EmployeeDataContext.Employees
+                .Include(e => e.Dependents)
+                .SingleOrDefaultAsync(e => e.EmployeeID == id);
+
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employee);
+        }
+
+
+        public async Task<ActionResult> AddDependent(int employeeId)
+        {
+            ViewData["EmployeeID"] = employeeId;
+            Employee employee = await EmployeeDataContext.Employees
+                .SingleOrDefaultAsync(e => e.EmployeeID == employeeId);
+            return View(employee);
+        }
+
+
+        private Task<Dependent> GetDependentAsync(int id)
+        {
+            return EmployeeDataContext.Dependents
+                .SingleOrDefaultAsync(d => d.DependentID == id);
+        }
+    }
+}
