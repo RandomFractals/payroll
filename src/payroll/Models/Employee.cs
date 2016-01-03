06ae8b83e4adfe0b5494201b0b44741e9ed458d7
Taras Novak <taras.novak@randomfractals.com>
2016-01-03 12:49:07 -0600

    added quick and dirty deductions calc to Employee for now

--- src/payroll/Models/Employee.cs
+++ src/payroll/Models/Employee.cs
@@ -5,8 +5,10 @@ namespace payroll.Models
 {
     public class Employee : Person
     {
-        // default salary to: 2000/check * 26 checks/year
-        public static decimal DefaultSalary = 2000 * 26;
+        // TODO: move these business rules to payroll service facade later
+        public static decimal DefaultSalary = 2000 * 26; // $2000/check * 26 checks/year
+        public static decimal DeductionPerEmployee = 1000; // $1000/year
+        public static decimal DeductionPerDependent = 500; // $500/year
 
         public int EmployeeID { get; set; }
 
@@ -19,6 +21,29 @@ namespace payroll.Models
 
         [Display(Name = "Dependents")]
         public ICollection<Dependent> Dependents { get; set; }
+
+        [Display(Name = "Deductions")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        //[NotMapped] - not in ef7 yet
+        public virtual decimal Deductions
+        {
+            get
+            {
+                // TODO: move this business logic to payroll service facade later
+                _deduction = DeductionPerEmployee;
+                if ( FirstName.StartsWith("A") )
+                {
+                    _deduction *= .01m; // 10% discount
+                }
+
+                return _deduction;
+            }
+            set {
+                _deduction = value;
+            }
+        }
+        private decimal _deduction;
+
 
         public Employee()
         {
--- src/payroll/Models/EmployeeDataContext.cs
+++ src/payroll/Models/EmployeeDataContext.cs
@@ -5,6 +5,12 @@ namespace payroll.Models
     public class EmployeeDataContext: DbContext
     {
         public DbSet<Employee> Employees { get; set;  }
-        public DbSet<Dependent> Dependents { get; set;  }
+        public DbSet<Dependent> Dependents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Ignore(e => e.Deductions);
+        }
     }
 }
