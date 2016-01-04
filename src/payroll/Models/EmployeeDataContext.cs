869aeb46b605057ed50beff490dc2805da21ff81
Taras Novak <taras.novak@randomfractals.com>
2016-01-04 01:36:26 -0600

    added db.ensurecreated for empl. data context

--- src/payroll/Models/EmployeeDataContext.cs
+++ src/payroll/Models/EmployeeDataContext.cs
@@ -4,6 +4,11 @@ namespace payroll.Models
 {
     public class EmployeeDataContext: DbContext
     {
+        public EmployeeDataContext()
+        {
+            Database.EnsureCreated();
+        }
+
         public DbSet<Employee> Employees { get; set;  }
         public DbSet<Dependent> Dependents { get; set; }
 
