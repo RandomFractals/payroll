f92b6213050f8d1dcb5d7dd527ea0a691b2e291c
Taras Novak <taras.novak@randomfractals.com>
2016-01-04 01:52:58 -0600

    added proper data ns and employee repo interface

---
+++ src/payroll/Data/IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using payroll.Models;
+
+namespace payroll.Data
+{
+    interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+
+        Task<bool> AddEmployeeAsync(Employee newEmployee);
+
+        Task<bool> UpdateEmployeeAsync(Employee employee);
+
+        Task<Employee> GetEmployeeByIdAsync(int id);
+
+        Task<bool> DeleteEmployeeAsync(int id);
+    }
+}
